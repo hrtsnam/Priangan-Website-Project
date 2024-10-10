ce52755ff5b64d4a3a3425afaf572536689e3eb9
Harits Nur Allam Muhammad <hrtsnam@gmail.com>
2024-10-10 23:48:20 +0700

    update rangka web

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function sayHello() {
+	let check = prompt("Halo cik, passwordnya dulu \n\nhint: priangan esport\n");
+	if (check=="protprotprot") {
+		window.alert("Selamat datang cik")
+	} else {
+		document.write("niga")
+	}
+}

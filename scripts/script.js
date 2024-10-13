38450389c267f780e807f27b3f397aae3555643a
Harits Nur Allam Muhammad <hrtsnam@gmail.com>
2024-10-13 22:18:13 +0700

    navbar downdate

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
-function sayHello() {
-	let check = prompt("Halo cik, passwordnya dulu \n\nhint: priangan esport\n");
-	if (check=="protprotprot") {
-		window.alert("Selamat datang cik")
-	} else {
-		document.write("Kupunya satu mimpi\nkutanamkan di hati\n\n\n\n\n\nquaso")
-	}
-}
+// function sayHello() {
+	// let check = prompt("Halo cik, passwordnya dulu \n\nhint: priangan esport\n");
+	// if (check=="protprotprot") {
+		// window.alert("Selamat datang cik")
+	// } else {
+		// document.write("Kupunya satu mimpi\nkutanamkan di hati\n\n\n\n\n\nquaso")
+	// }
+// }

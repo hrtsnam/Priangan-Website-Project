6df936fc2458db80a0e79f8cf128eb6bf646e82f
Harits Nur Allam Muhammad <hrtsnam@gmail.com>
2024-10-14 16:09:18 +0700

    scroll function and content update

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,4 @@
-// function sayHello() {
-	// let check = prompt("Halo cik, passwordnya dulu \n\nhint: priangan esport\n");
-	// if (check=="protprotprot") {
-		// window.alert("Selamat datang cik")
-	// } else {
-		// document.write("Kupunya satu mimpi\nkutanamkan di hati\n\n\n\n\n\nquaso")
-	// }
-// }
+function scrollTo(thisSection) {
+	let this = document.getElementById('thisSection')
+	this.scrollIntoView({behavior: smooth})
+}

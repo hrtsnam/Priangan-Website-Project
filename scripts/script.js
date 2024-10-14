4579aed9d0bd4ff25f4827b6df129037fcef80b5
Harits Nur Allam Muhammad <hrtsnam@gmail.com>
2024-10-14 22:07:41 +0700

    revisi navbar

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
 function scrollTo(thisSection) {
-	let this = document.getElementById('thisSection')
-	this.scrollIntoView({behavior: smooth})
+	let this = document.getElementById('thisSection');
+	this.scrollIntoView({behavior: smooth});
+}
+
+function getInfo(inputId, inputField) {
+	let userInput = document.getElementById('inputId').value;
+	document.getElementById('inputField').innerHTML = userInput;
 }
